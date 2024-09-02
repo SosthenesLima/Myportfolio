821b4d38e30b886f04b49397780e5718c5b97237
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-09-02 20:31:06 -0300

    novo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Sósthenes O. Lima 2024',
+  title: 'Sósthenes O. Lima 2025',
   description: 'Sósthenes O. Lima is a software engineer...',
 };
 
